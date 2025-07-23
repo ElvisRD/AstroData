44a6fa5b767fb25177a4ae543baa0bbcf300ec48
ElvisRD <animeamv682@gmail.com>
2025-07-23 17:03:47 -0400

    Added urls api nasa

---
+++ src/app/services/nasa-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NasaApiKey } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NasaServiceService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAstronomyPictureOfTheDay() {
+    return this.http.get('https://api.nasa.gov/planetary/apod?api_key=' + NasaApiKey);
+  }
+
+  getAsteroidsNeoFeed() {
+    return this.http.get('https://api.nasa.gov/neo/rest/v1/feed?api_key=' + NasaApiKey);
+  }
+
+  getMarsRoverPhotos(sol: number, camera: string) {
+    return this.http.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camera}&api_key=${NasaApiKey}`);
+  }
+
+  getNasaImagesAndVideos() {
+    return this.http.get('https://images-api.nasa.gov/search?media_type=image,video&api_key=' + NasaApiKey);
+  }
+
+  getOpenScienceData(){
+    return this.http.get('https://osdr.nasa.gov/osdr/data/osd/files/87.1')
+  }
+
+
+
+
+}
