4833edded5f267d28e4c8b2a43ea312992e94165
ElvisRD <elvisAppsdev@gmail.com>
2025-08-01 22:51:44 -0400

    added new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { EarthComponent } from './pages/earth/earth.component';
+import { MarsComponent } from './pages/mars/mars.component';
+import { AsteroidsComponent } from './pages/asteroids/asteroids.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
+  {path: 'earth', component: EarthComponent},
+  {path: 'mars', component: MarsComponent},
+  {path: 'asteroids', component: AsteroidsComponent},
   {path: '**', redirectTo: '/home'}
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TooltipModule],
+  imports: [TooltipModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
