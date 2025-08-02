cf706d0bcd607e0b3f8301c848f01bdb6a3608be
ElvisRD <elvisAppsdev@gmail.com>
2025-08-01 22:46:09 -0400

    added news pages

---
+++ src/app/pages/mars/mars.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mars',
+  imports: [],
+  templateUrl: './mars.component.html',
+  styleUrl: './mars.component.scss'
+})
+export class MarsComponent {
+
+}
